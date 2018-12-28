b13985648b32a43bc23cd305c1fad441865da69e
Marcin Armatys <marcin.armatys@gmail.com>
2018-12-28 22:02:05 +0100

    Initial commit - npm init & karma init
    * Calling npm init
    * removing main from package.json
    * Adding LICENCE
    * Adding gitignore
    * npm install karma karma-jasmine karma-chrome-launcher jasmine-core --save-dev
    * npx karma init
    ** Which testing framework do you want to use ?
    ** > jasmine
    ** Do you want to use Require.js ?
    ** > no
    ** Do you want to capture any browsers automatically ?
    ** > ChromeHeadless
    ** >
    ** What is the location of your source and test files ?
    ** > src/**/*.js
    ** WARN [init]: There is no file matching this pattern.
    ** >
    ** Should any of the files included by the previous patterns be excluded ?
    ** >
    ** Do you want Karma to watch all the files and run the tests on change ?
    ** > yes
    ** Config file generated at "${REPO}/karma.conf.js".

---
+++ karma.conf.js
@@ -0,0 +1,21 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'src/**/*.js'
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+    concurrency: Infinity
+  })
+};
